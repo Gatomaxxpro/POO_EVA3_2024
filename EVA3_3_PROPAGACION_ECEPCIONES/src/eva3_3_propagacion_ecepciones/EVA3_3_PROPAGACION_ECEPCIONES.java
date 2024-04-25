f36877dfc6dd808b4d627c03d4069193c1be1154
Maximiliano <143568715+Gatomaxxpro@users.noreply.github.com>
2024-04-24 22:09:36 -0600

    Add files via upload

---
+++ EVA3_3_PROPAGACION_ECEPCIONES/src/eva3_3_propagacion_ecepciones/EVA3_3_PROPAGACION_ECEPCIONES.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_3_propagacion_ecepciones;
+
+/**
+ *
+ * @author MAX
+ */
+public class EVA3_3_PROPAGACION_ECEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+        
+    }
+    public static void B(){
+        C();
+        
+        
+    }
+    public static void C(){
+        int x = 10, y=0;
+        int resu=x/y;
+        System.out.println(resu);
+       
+    }
+}

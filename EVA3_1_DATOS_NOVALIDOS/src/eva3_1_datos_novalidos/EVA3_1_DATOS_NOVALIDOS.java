bd7ff8adec607ec0525d221309e84cf42c8eb42e
Maximiliano <143568715+Gatomaxxpro@users.noreply.github.com>
2024-04-24 22:06:58 -0600

    Add files via upload

---
+++ EVA3_1_DATOS_NOVALIDOS/src/eva3_1_datos_novalidos/EVA3_1_DATOS_NOVALIDOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_datos_novalidos;
+
+/**
+ *
+ * @author MAX
+ */
+public class EVA3_1_DATOS_NOVALIDOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nm=0;
+        int numero=0;
+        int resu;
+        resu=nm/numero;
+        System.out.println("el numero es valido"+resu);
+    }
+    
+}

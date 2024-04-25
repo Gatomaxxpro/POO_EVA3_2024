364105552c75eb7282adcbab92b1289b6b551ddd
Maximiliano <143568715+Gatomaxxpro@users.noreply.github.com>
2024-04-24 22:08:58 -0600

    Add files via upload

---
+++ EVA3_EXCEPCIONES/src/eva3_excepciones/EVA3_EXCEPCIONES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_excepciones;
+
+/**
+ *
+ * @author MAX
+ */
+public class EVA3_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //persona Persona=null;
+       // System.out.println(Persona.toString());
+        // TODO code application logic here
+        
+        //int ARR[]=new int [10];// n=10
+        //0 es la primer posicion n-1 es la ultima (9)
+       // ARR[15]=100;
+       //Arimeticaexception: Divicion entre cero 
+       int x =10, y=0;
+       int resu=x/y;
+        System.out.println(resu);
+    }
+    
+}
